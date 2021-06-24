f29332ec14bf083b6035babbc49596a552f9f75c
John Chapple <4251635-johnchapple@users.noreply.gitlab.com>
2021-06-23 19:20:31 -0500

    remove slash on socket connection string

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 //  "wss://squid.chat/"
 //  http://127.0.0.1:5000/sockets/
 
-export const socket = io("https://squid.chat/sockets/"  , {
+export const socket = io("https://squid.chat/sockets"  , {
     extraHeaders: {
       tasty_token: `Bearer ${localStorage.getItem('jwt')}`
     }
