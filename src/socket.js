083b07e33a63a567549a555975849215f831e545
John Chapple <4251635-johnchapple@users.noreply.gitlab.com>
2021-06-29 10:55:54 -0500

    sockets working locally, but cannot receive messages

--- src/socket.js
+++ src/socket.js
@@ -4,10 +4,25 @@ import io from "socket.io-client";
 //  "wss://squid.chat/"
 //  http://127.0.0.1:5000/sockets/
 
-export const socket = io("https://squid.chat/sockets"  , {
-    extraHeaders: {
-      tasty_token: `Bearer ${localStorage.getItem('jwt')}`
-    }
-  });
+// https://squid.chat/sockets
+
+export const socket = io.connect("http://127.0.0.1:5000"  , {
+  extraHeaders: {
+    tasty_token: `Bearer ${localStorage.getItem('jwt')}`
+  }
+})
+
+// const manager = new Manager("ws://127.0.0.1:5000/sockets", {
+//   transports: ["websocket"],
+//   extraHeaders: {
+//     tasty_token: `Bearer ${localStorage.getItem('jwt')}`
+//   }
+// })
+
+// export const socket = manager.socket("/", {
+
+//   });
 
 socket.emit('new_session')
+
+// const s = new WebSocket('https://squid.chat/sockets')
