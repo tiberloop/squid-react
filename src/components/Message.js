15c21d759aa742240b01082f093a99bc03548633
John Chapple <volatileelixir@protonmail.com>
2021-07-05 14:49:49 -0500

    styling for avatar on messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,23 +3,27 @@ import axios from "axios";
 
 function Message(props) {
   const { message, index } = props
-  const [src, setSrc] = useState('')
+  const [src, setSrc] = useState('https://res.cloudinary.com/dk-find-out/image/upload/q_70,c_pad,w_1200,h_630,f_auto/DCTM_Penguin_UK_DK_AL639403_k3qity.jpg')
 
   if (index === 1 ) {
-      axios.get("uploads/0803_florida-recount.jpg").then(res => {
-      console.log(res.data)
+      axios.get("/uploads/squidimage.jpg").then(res => {
+      console.log('image', res.data)
       setSrc(res.data)
     })
   }
 
   return (
-  <div className={`p-1 ${(index % 2) && 'bg-gray-100'}`}>
+  <div className={`flex p-1 ${(index % 2) && 'bg-gray-100'}`}>
     {/* <div className="bg-green-300" style={{ height: '32px', width: '32px' }}></div> */}
-    <img src={src} style={{ height: '32px', width: '32px' }} />
-    <p><strong>{message.username}</strong> <i>{message.time_sent}</i></p>
-    <p>
-      {message.text}
-    </p>
+    <div>
+      <img src={src} alt="Avatar" style={{ height: '32px', width: '32px' }} />
+    </div>
+    <div className="ml-1">
+      <p><strong>{message.username}</strong> <i>{message.time_sent}</i></p>
+      <p>
+        {message.text}
+      </p>
+    </div>
   </div>
   )
 }
