d4c1dc08a907a22fc8d3328bb769180e4d75aae6
John <johnchapple@ksu.edu>
2021-07-11 09:00:07 -0500

    dont show return to room button on desktop

--- src/Main.js
+++ src/Main.js
@@ -156,7 +156,7 @@ function Main() {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </button>
-          {currentRoom && (
+          {currentRoom && isMobile && (
           <button onClick={() => setMenuOrChat(true)} className="px-2 hover:bg-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
