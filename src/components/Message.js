6b1da91d789187dc80bb5f3abb76f24463808bec
John <johnchapple@ksu.edu>
2021-07-10 21:53:03 -0500

    format chat message and stock working avatar code

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { format } from 'date-fns'
 
 function Message(props) {
   const { message, index } = props
   const [src, setSrc] = useState('https://res.cloudinary.com/dk-find-out/image/upload/q_70,c_pad,w_1200,h_630,f_auto/DCTM_Penguin_UK_DK_AL639403_k3qity.jpg')
 
-  // if (index === 1 ) {
-  //     axios.get("/uploads/squidimage.jpg").then(res => {
-  //     console.log('image', res.data)
-  //     setSrc(res.data)
+  // if (index === 1) {
+  //     axios.get(`/uploads/${message.avatar_id}`, { responseType: "blob" }).then(res => {
+  //     const srcurl = URL.createObjectURL(res.data)
+  //     setSrc(srcurl)
   //   })
   // }
+
+  const timeSentFormatted = format(new Date(message.time_sent), 'p')
 
   return (
   <div className={`flex p-1 ${(index % 2) && 'bg-gray-100'}`}>
     {/* <div className="bg-green-300" style={{ height: '32px', width: '32px' }}></div> */}
-    <div>
+    <div className="bg-gray-200 rounded">
       <img src={src} alt="Avatar" style={{ height: '32px', width: '32px' }} />
     </div>
     <div className="ml-1">
-      <p><strong>{message.username}</strong> <i>{message.time_sent}</i></p>
+      <p><strong>{message.username}</strong> <i>{timeSentFormatted}</i></p>
       <p>
         {message.text}
       </p>
