9821bbac60525aff76d87aaa8f2b727a2ccdc319
John Jack <jklewis99@gmail.com>
2021-07-24 01:52:18 -0500

    added dark:class condition to input elements

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -67,19 +67,19 @@ function Login() {
         <h1 className="text-xl mb-8">Sign Up</h1>
         <label className="flex mb-2 items-center">
         <span className="mr-2">Username</span>
-          <input className="flex-grow p-1 border border-gray-200" value={registerUsername} onChange={(e) => { setRegisterUsername(e.target.value) }} type="text" id="registerUsername" />
+          <input className="flex-grow p-1 border border-gray-200 dark:bg-primaryDark" value={registerUsername} onChange={(e) => { setRegisterUsername(e.target.value) }} type="text" id="registerUsername" />
         </label>
         <label className="mb-2 flex items-center">
           <span className="mr-2">Password</span>
-          <input className="flex-grow p-1 border border-gray-200" value={registerPassword} onChange={(e) => { setRegisterPassword(e.target.value) }} type="password" id="registerPassword" />
+          <input className="flex-grow p-1 border border-gray-200 dark:bg-primaryDark" value={registerPassword} onChange={(e) => { setRegisterPassword(e.target.value) }} type="password" id="registerPassword" />
         </label>
         <label className="mb-2 flex items-center">
           <span className="mr-2">Email</span>
-          <input className="flex-grow p-1 border border-gray-200" value={registerEmail} onChange={(e) => { setRegisterEmail(e.target.value) }} type="email" id="registerEmail" />
+          <input className="flex-grow p-1 border border-gray-200 dark:bg-primaryDark" value={registerEmail} onChange={(e) => { setRegisterEmail(e.target.value) }} type="email" id="registerEmail" />
         </label>
         <label className="mb-2 flex items-center">
           <span className="mr-2">Full Name</span>
-          <input className="flex-grow p-1 border border-gray-200" value={registerName} onChange={(e) => { setRegisterName(e.target.value) }} type="text" id="registerName" />
+          <input className="flex-grow p-1 border border-gray-200 dark:bg-primaryDark" value={registerName} onChange={(e) => { setRegisterName(e.target.value) }} type="text" id="registerName" />
         </label>
         <div className="flex mt-8 justify-between items-center">
           <button className="p-1 px-3 rounded hover:bg-blue-800 bg-blue-900 text-white" type="button" onClick={() => signup()}>
