1ce2b62ed69834ddf3acf4fc35241b4de9f011db
John <johnchapple@ksu.edu>
2021-08-08 18:21:31 -0500

    better users list

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -4,7 +4,7 @@ import { faGhost, faHeartbeat, faComment } from '@fortawesome/free-solid-svg-ico
 import { ISquidMessage, ISquidRoom, ISquidUser } from "utils/apiObjects";
 import { useAppSelector } from "hooks";
 import { getUsersList, getUserAvatar, getDM } from "utils/apiHelper";
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 function UsersList() {
   const history = useHistory();
@@ -24,7 +24,7 @@ function UsersList() {
           }));
       setAllUsers(users.filter((user) => (user.ID !== loggedInUser.ID)));
     }).then(r => setAllAvatars(avatars))
-  }, []);
+  }, [isMobile]);
 
   const navigateToUser = (user: ISquidUser) => {
     history.push({pathname:`/profile/${user.username}`, state: {userId: user.ID}});
@@ -53,15 +53,34 @@ function UsersList() {
         </div>
         <div className="border-b border-gray-300 squid-scrollable">
             {allUsers && allUsers.map((user: ISquidUser) => (
-              <div className="flex flex-row items-center px-2 py-0.5" key={user.username +"userslist"}>
-                <div className="relative w-9 h-9">
-                {allAvatars[user.ID] ? <img src={`data: image/png; base64,${allAvatars[user.ID]}`} className="flex-shrink-0 b-2 rounded-full w-8 h-8 min-w-8 min-h-8"/> : <span/>}
-                <FontAwesomeIcon className="absolute right-0 bottom-0 pl-0.5 pt-0.5" icon={isActive(user.ID) === true ? faHeartbeat  : faGhost} color={isActive(user.ID) === true ? "green" :"#444444"} />
-                </div>  
-                <button className="p-2 text-left block box-border" onClick={() => navigateToUser(user)}>
-                  {user.username}
+              <div className="flex flex-row justify-between items-center px-2 py-0.5" key={user.username +"userslist"}>
+                
+                <div className="flex truncate">
+                  <div className="relative w-9 h-9">
+                    <img src={`data: image/png; base64,${allAvatars[user.ID]}`} className="flex-shrink-0 object-cover b-2 rounded-full w-8 h-8 min-w-8 min-h-8"/>
+                    {/* <FontAwesomeIcon className="absolute right-0 bottom-0 pl-0.5 pt-0.5" icon={isActive(user.ID) === true ? faHeartbeat  : faGhost} color={isActive(user.ID) === true ? "green" :"#444444"} /> */}
+                    <div className={`absolute rounded w-2 h-2 right-0 bottom-0 pl-0.5 pt-0.5 ${isActive(user.ID) ? 'bg-green-600' : 'bg-gray-400'}`} />
+                  </div>
+
+                  {/* history.push({pathname:`/profile/${user.username}`, state: {userId: user.ID}}); */}
+                  <Link 
+                    className="p-2 hover:underline truncate text-left block box-border"
+                    to={{
+                      pathname: `/profile/${user.username}`,
+                      state: {userId: user.ID}
+                    }}
+                  >
+                    {user.username}
+                  </Link>
+                  
+                  {/* <button className="hover:underline pointer-cursor p-2 text-left block box-border" onClick={() => navigateToUser(user)}>
+                    
+                  </button> */}
+                </div>
+
+                <button className="py-1 px-2 rounded hover:border-gray-500 border border-transparent" onClick={() => selectDM(user.ID)}>
+                  <FontAwesomeIcon className="ml-auto cursor-pointer" icon={faComment} />
                 </button>
-                <FontAwesomeIcon className="ml-auto cursor-pointer" icon={faComment} onClick={() => selectDM(user.ID)}/>
               </div>
             ))}
           </div>
