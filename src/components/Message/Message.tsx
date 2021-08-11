0afd4f612c3a1c8cd4a43036856910e8c56ece14
John <johnchapple@ksu.edu>
2021-08-11 09:07:59 -0500

    fix messages formatting and messages images

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -16,7 +16,7 @@ function Message(props: IMessageProps) {
   
   const [src, setSrc] = useState('https://res.cloudinary.com/dk-find-out/image/upload/q_70,c_pad,w_1200,h_630,f_auto/DCTM_Penguin_UK_DK_AL639403_k3qity.jpg')
   const [open, setOpen] = useState(false)
-  const [img, setImg] = useState();
+  const [img, setImg] = useState('');
 
   useEffect(() => {
     // if (message.image_id) {
@@ -35,6 +35,16 @@ function Message(props: IMessageProps) {
   //   })
   // }
   const [avatars, setAvatars] = useStore('avatars')
+
+
+  useEffect(() => {
+    if (message.image_id !== 'None' && message.image_id) {
+      axios.get(`/uploads/${message.image_id}`, { responseType: "blob" })
+        .then(res => {
+          setImg(URL.createObjectURL(res.data))
+        })
+    }
+  }, [message.image_id])
 
   useEffect(() => {
     const avatar = avatars.find((a: any) => a.userId === message.user_id)
@@ -70,17 +80,19 @@ function Message(props: IMessageProps) {
   const timeSentFormatted = formatTimeSent(message.time_sent);
 
   return (
-  <div className={`flex p-1 items-center ${(index % 2) && 'bg-gray-100 dark:bg-primaryDarkContrast'}`}>
+  <div className={`flex p-1 ${(index % 2) && 'bg-gray-100 dark:bg-primaryDarkContrast'}`}>
     <UserCardModal open={open} setOpen={setOpen} userId={message.user_id} />
-    <div onClick={() => setOpen(true)} className="hover:border-white border border-transparent cursor-pointer rounded">
-      <img className="rounded object-cover" src={src} alt="Avatar" style={{ height: '32px', width: '32px' }} />
+    <div className="h-full flex-shrink-0">
+      <div onClick={() => setOpen(true)} className="hover:border-white border border-transparent cursor-pointer rounded">
+        <img className="rounded object-cover" src={src} alt="Avatar" style={{ height: '32px', width: '32px' }} />
+      </div>
     </div>
     <div className="message-sender ml-1">
       <p><strong className="hover:underline cursor-pointer" onClick={() => setOpen(true)}>{message.username}</strong> <small>{timeSentFormatted}</small></p>
       <p className='message-text'>
         {message.text}
       </p>
-      {img && <img className="" src={`data: image/png; base64,${img}`}/>}
+      {img && <img alt="attachment" src={img}/>}
     </div>
   </div>
   )
